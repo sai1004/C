0c6da948d47e8c191441f47cd8b2f80960520556
saikiran <35389534+sai1004@users.noreply.github.com>
2019-08-26 16:51:55 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main (
+
+  int l, b, area;
+  
+  printf("\n Enter the Lenth: " );  
+  scanf(" %d ", &l );
+  printf("\n Enter the Bredth: ");
+  scanf(" %d ", &b );
+  area = l * b;
+  printf("\n the area is %d ", area );
+  
+  
+);
