24fd57812aa8826fb4e32877e6ea1b29f87dcf20
saikiran <35389534+sai1004@users.noreply.github.com>
2019-11-14 22:43:09 +0530

    Create addFunction.c

---
+++ addFunction.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main() {
+ 
+ int num1 , int num2, int total; 
+ 
+ printf("Enter The num1 Value: ");
+ scanf(" %d ", &num1);
+ printf("Enter The num2 Value: ");
+ scanf(" %d ", &num2);
+ 
+ total = add(num1,num2);
+ printf("The Total is %d :", total);
+
+};
+
+int add(int m, int n) {
+
+    int sum;
+    sum = m + n;
+    
+    return sum;
+
+};
