e276cb924cbc9c257657cea7d19bce0717ea1fa5
sai1004 <saikiran.bavandla@gmail.com>
2019-12-01 20:25:00 +0530

    commit

--- chapters/chapter1/statements.c
+++ chapters/chapter1/statements.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+
+int main()
+{
+
+    int number = 0;
+
+    printf("Enter a Number: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0)
+    {
+        printf(" %d is the even number \n", number);
+    }
+    else
+    {
+        printf(" %d is odd number \n", number);
+    }
+
+    return 0;
+}
